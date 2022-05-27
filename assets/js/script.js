b01bb9b53e6c065da83ea5542dd4d1beac1ffa52
clayc03 <clayc03@gmail.com>
2022-05-27 15:44:00 +0000

    Add const variables to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const buttons = document.getElementsByClassName("button");
+const playerScore = document.getElementById("your-score");
+const computerScore = document.getElementById("computer-score");
+const playerChoice = document.getElementById("your-choice");
+const computerChoice = document.getElementById("computer-choice");
+const gameResult = document.getElementById("game-result");
+const yourPick = ["rock", "paper", "scissors"];
