aef6338bdeb73ce9d18f8da9fdc46d93a774b991
clayc03 <clayc03@gmail.com>
2022-05-30 11:49:59 +0000

    Delete js code and start with new code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,6 @@
-/**
- * Const for DOM Elements
- */
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoiceDisplay
 
-const buttons = document.getElementsByClassName("button");
-const playerScore = document.getElementById("your-score");
-const computerScore = document.getElementById("computer-score");
-const yourChoice = document.getElementById("your-choice");
-const computerChoice = document.getElementById("computer-choice");
-const gameResult = document.getElementById("game-result");
-const yourPick = ["rock", "paper", "scissors"];
-
-/** Event Listeners for buttons */
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let yourChoice = this.getAttribute("data-choice");
-        playGame(yourChoice)
-    });
-}
-
-/** Game function */
-
-function playGame(yourChoice) {
-
-    yourChoice.src = `assets/images/game-image/${choices[yourChoice]}.png`;
-    yourChoice.alt = choices[yourChoice];
-
-    let computerChoice = Math.floor(Math.random() * 3);
-
-    computerChoice.src = `assets/images/game-image/${choices[computerChoice]}.png`;
-    computerChoice.alt = choice[computerChoice];
-
-    let result = checkWinner(choices[computerChoice], choices[yourChoice]);
-
-    updateScores(result);
-}
