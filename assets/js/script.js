d99ed7e116edd6c072f23bd76eb5258f4fd73c25
clayc03 <clayc03@gmail.com>
2022-05-31 12:07:33 +0000

    Amend html file, add more javascript code to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,42 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     generateComputerChoice()
 }))
 
+function generateComputerChoice () {
+const randomNumber = Math.floor(Math.random() * 3) + 1
+
+if (randomNumber === 1) {
+    computerChoice = "rock"
+}
+if (randomNumber === 2) {
+    computerChoice = "scissors"
+}
+if (randomNumber === 3) {
+    computerChoice = "paper"
+}
+computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+if (computerChoice === userChoice) {
+    result = "A Draw!"
+}
+if (computerChoice === "rock" && userChoice === "paper") {
+    result = "You Win!"
+}
+if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "You Lose!"
+}
+if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "You Win!"
+}
+if (computerChoice === "paper" && userChoice === "rock") {
+    result = "You Lose!"
+}
+if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "You Win!"
+}
+if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "You Lose!"
+}
+resultDisplay.innerHTML = result
+}
